1c08ccc9b564f4e1a980467c9600ef4c0e45780e
Rylan <1217013295@qq.com>
2023-12-31 08:21:36 +0800

    💄 自定义 Loading 动画

--- components/Comment.js
+++ components/Comment.js
@@ -1,9 +1,9 @@
-import dynamic from 'next/dynamic'
 import Tabs from '@/components/Tabs'
+import { siteConfig } from '@/lib/config'
 import { isBrowser, isSearchEngineBot } from '@/lib/utils'
+import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router'
 import Artalk from './Artalk'
-import { siteConfig } from '@/lib/config'
 
 const WalineComponent = dynamic(
   () => {
@@ -94,11 +94,20 @@ const Comment = ({ siteInfo, frontMatter, className }) => {
   if (!frontMatter) {
     return (
       <>
-        <br />
-        <br />
-        稍等一会 | Loading...
-        <br />
-        <br />
+        {/* 全屏白色遮罩 */}
+        <div className="fixed inset-0 bg-white z-10"></div>
+        <div className="fixed inset-0 flex justify-center items-center z-50">
+          {/* 闪烁动画 */}
+          <div className="relative p-2 border-8 border-slate-200 rounded-full bg-white" style={{ width: '50vmin', height: '50vmin' }}>
+            <img
+              src="./favicon.png"
+              className="animate-blink relative rounded-full w-full h-full"
+              alt="Loading"
+            />
+            {/* 小圆形固定在大圆形的右下角 */}
+            <div className="absolute bottom-0 right-0 mb-1 mr-1 md:mb-2 md:mr-2 lg:mb-4 lg:mr-4 h-10 w-10 md:h-12 md:w-12 lg:h-16 lg:w-16 bg-green-500 rounded-full border-4 border-slate-200"></div>
+          </div>
+        </div>
       </>
     )
   }
@@ -107,9 +116,8 @@ const Comment = ({ siteInfo, frontMatter, className }) => {
     <div
       key={frontMatter?.id}
       id='comment'
-      className={`comment mt-5 text-gray-800 dark:text-gray-300 ${
-        className || ''
-      }`}
+      className={`comment mt-5 text-gray-800 dark:text-gray-300 ${className || ''
+        }`}
     >
       <Tabs>
         {COMMENT_ARTALK_SERVER && (
