aa22a9bbd5822443f88f2c4e406965202cdcb159
Rylan <1217013295@qq.com>
2024-01-06 00:51:54 +0800

    💄 修改日期展示格式

--- themes/hexo/components/BlogPostCardInfo.js
+++ themes/hexo/components/BlogPostCardInfo.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import TagItemMini from './TagItemMini'
 import TwikooCommentCount from '@/components/TwikooCommentCount'
 import { siteConfig } from '@/lib/config'
-import { formatDateFmt } from '@/lib/formatDate'
+// import { formatDateFmt } from '@/lib/formatDate'
 
 /**
  * 博客列表的文字内容
@@ -11,84 +11,87 @@ import { formatDateFmt } from '@/lib/formatDate'
  * @returns
  */
 export const BlogPostCardInfo = ({ post, showPreview, showPageCover, showSummary }) => {
-  return <div className={`flex flex-col justify-between lg:p-6 p-4  ${showPageCover && !showPreview ? 'md:w-7/12 w-full md:max-h-60' : 'w-full'}`}>
-       <div>
-         {/* 标题 */}
-         <Link
-            href={`${siteConfig('SUB_PATH', '')}/${post.slug}`}
-            passHref
-            className={`line-clamp-2 replace cursor-pointer text-2xl ${showPreview ? 'text-center' : ''
-                } leading-tight font-normal text-gray-600 dark:text-gray-100 hover:text-indigo-700 dark:hover:text-indigo-400`}>
-
-            <span className='menu-link '>{post.title}</span>
-
-        </Link>
-
-        {/* 分类 */}
-        { post?.category && <div
-            className={`flex mt-2 items-center ${showPreview ? 'justify-center' : 'justify-start'
-                } flex-wrap dark:text-gray-500 text-gray-400 `}
-        >
+    return <div className={`flex flex-col justify-between lg:p-6 p-4  ${showPageCover && !showPreview ? 'md:w-7/12 w-full md:max-h-60' : 'w-full'}`}>
+        <div>
+            {/* 标题 */}
             <Link
-                href={`/category/${post.category}`}
+                href={`${siteConfig('SUB_PATH', '')}/${post.slug}`}
                 passHref
-                className="cursor-pointer font-light text-sm menu-link hover:text-indigo-700 dark:hover:text-indigo-400 transform">
+                className={`line-clamp-2 replace cursor-pointer text-2xl ${showPreview ? 'text-center' : ''
+                    } leading-tight font-normal text-gray-600 dark:text-gray-100 hover:text-indigo-700 dark:hover:text-indigo-400`}>
 
-                <i className="mr-1 far fa-folder" />
-                {post.category}
+                <span className='menu-link '>{post.title}</span>
 
             </Link>
 
-            <TwikooCommentCount className='text-sm hover:text-indigo-700 dark:hover:text-indigo-400' post={post}/>
-        </div>}
+            {/* 分类 */}
+            {post?.category && <div
+                className={`flex mt-2 items-center ${showPreview ? 'justify-center' : 'justify-start'
+                    } flex-wrap dark:text-gray-500 text-gray-400 `}
+            >
+                <Link
+                    href={`/category/${post.category}`}
+                    passHref
+                    className="cursor-pointer font-light text-sm menu-link hover:text-indigo-700 dark:hover:text-indigo-400 transform">
 
-          {/* 摘要 */}
-          {(!showPreview || showSummary) && !post.results && (
-            <p className="line-clamp-2 replace my-3 text-gray-700  dark:text-gray-300 text-sm font-light leading-7">
-                {post.summary}
-            </p>
-          )}
+                    <i className="mr-1 far fa-folder" />
+                    {post.category}
 
-        {/* 搜索结果 */}
-        {post.results && (
-            <p className="line-clamp-2 mt-4 text-gray-700 dark:text-gray-300 text-sm font-light leading-7">
+                </Link>
+
+                <TwikooCommentCount className='text-sm hover:text-indigo-700 dark:hover:text-indigo-400' post={post} />
+            </div>}
+
+            {/* 摘要 */}
+            {(!showPreview || showSummary) && !post.results && (
+                <p className="line-clamp-2 replace my-3 text-gray-700  dark:text-gray-300 text-sm font-light leading-7">
+                    {post.summary}
+                </p>
+            )}
+
+            {/* 搜索结果 */}
+            {post.results && (
+                <p className="line-clamp-2 mt-4 text-gray-700 dark:text-gray-300 text-sm font-light leading-7">
                     {post.results.map((r, index) => (
                         <span key={index}>{r}</span>
                     ))}
-            </p>
-        )}
-        {/* 预览 */}
-        {showPreview && (
-            <div className="overflow-ellipsis truncate">
-                <NotionPage post={post} />
-            </div>
-        )}
+                </p>
+            )}
+            {/* 预览 */}
+            {showPreview && (
+                <div className="overflow-ellipsis truncate">
+                    <NotionPage post={post} />
+                </div>
+            )}
 
-       </div>
+        </div>
 
-       <div>
-         {/* 日期标签 */}
-         <div className="text-gray-400 justify-between flex">
-            {/* 日期 */}
-            <Link
-                href={`/archive#${formatDateFmt(post?.publishDate, 'yyyy-MM')}`}
-                passHref
-                className="font-light menu-link cursor-pointer text-sm leading-4 mr-3">
+        <div>
+            {/* 日期标签 */}
+            <div className="text-gray-400 justify-between flex">
+                {/* 日期 */}
+                {/* <Link
+                    href={`/archive#${formatDateFmt(post?.publishDate, 'yyyy-MM')}`}
+                    passHref
+                    className="font-light menu-link cursor-pointer text-sm leading-4 mr-3">
+                    <i className="far fa-calendar-alt mr-1" />
+                    {post?.publishDay || post.lastEditedDay}
+                </Link> */}
+                <span className="font-light text-sm leading-4 mr-3">
+                    <i className="far fa-calendar-alt mr-1" />
+                    {post?.publishDay}
+                    {post.finished_date && (<> - {post.finished_date} </>)}
+                </span>
 
-                <i className="far fa-calendar-alt mr-1" />
-                {post?.publishDay || post.lastEditedDay}
-
-            </Link>
-
-            <div className="md:flex-nowrap flex-wrap md:justify-start inline-block">
-                <div>
-                    {' '}
-                    {post.tagItems?.map(tag => (
-                        <TagItemMini key={tag.name} tag={tag} />
-                    ))}
+                <div className="md:flex-nowrap flex-wrap md:justify-start inline-block">
+                    <div>
+                        {' '}
+                        {post.tagItems?.map(tag => (
+                            <TagItemMini key={tag.name} tag={tag} />
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
-       </div>
-       </div>
+    </div>
 }
