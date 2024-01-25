02d35d4d27c2c45f3a0eda93ae6bf4ed4d3e740c
Rylan <1217013295@qq.com>
2024-01-25 15:42:52 +0800

    💩 修改 404 逻辑

--- themes/hexo/index.js
+++ themes/hexo/index.js
@@ -44,63 +44,63 @@ const LayoutBase = props => {
   const { onLoading, fullWidth } = useGlobal()
 
   return (
-        <div id='theme-hexo'>
-            {/* 网页SEO */}
-            <CommonHead meta={meta}/>
-            <Style/>
+    <div id='theme-hexo'>
+      {/* 网页SEO */}
+      <CommonHead meta={meta} />
+      <Style />
 
-            {/* 顶部导航 */}
-            <TopNav {...props} />
+      {/* 顶部导航 */}
+      <TopNav {...props} />
 
-            {/* 顶部嵌入 */}
+      {/* 顶部嵌入 */}
+      <Transition
+        show={!onLoading}
+        appear={true}
+        enter="transition ease-in-out duration-700 transform order-first"
+        enterFrom="opacity-0 -translate-y-16"
+        enterTo="opacity-100"
+        leave="transition ease-in-out duration-300 transform"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0 translate-y-16"
+        unmount={false}
+      >
+        {headerSlot}
+      </Transition>
+
+      {/* 主区块 */}
+      <main id="wrapper" className={`${siteConfig('HEXO_HOME_BANNER_ENABLE', null, CONFIG) ? '' : 'pt-16'} bg-hexo-background-gray dark:bg-black w-full py-8 md:px-8 lg:px-24 min-h-screen relative`}>
+        <div id="container-inner" className={(JSON.parse(siteConfig('LAYOUT_SIDEBAR_REVERSE')) ? 'flex-row-reverse' : '') + ' w-full mx-auto lg:flex lg:space-x-4 justify-center relative z-10'} >
+          <div className={`${className || ''} w-full ${fullWidth ? '' : 'max-w-4xl'} h-full overflow-hidden`}>
+
             <Transition
-                show={!onLoading}
-                appear={true}
-                enter="transition ease-in-out duration-700 transform order-first"
-                enterFrom="opacity-0 -translate-y-16"
-                enterTo="opacity-100"
-                leave="transition ease-in-out duration-300 transform"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0 translate-y-16"
-                unmount={false}
+              show={!onLoading}
+              appear={true}
+              enter="transition ease-in-out duration-700 transform order-first"
+              enterFrom="opacity-0 translate-y-16"
+              enterTo="opacity-100"
+              leave="transition ease-in-out duration-300 transform"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 -translate-y-16"
+              unmount={false}
             >
-                {headerSlot}
+              {/* 主区上部嵌入 */}
+              {slotTop}
+
+              {children}
             </Transition>
+          </div>
 
-            {/* 主区块 */}
-            <main id="wrapper" className={`${siteConfig('HEXO_HOME_BANNER_ENABLE', null, CONFIG) ? '' : 'pt-16'} bg-hexo-background-gray dark:bg-black w-full py-8 md:px-8 lg:px-24 min-h-screen relative`}>
-                <div id="container-inner" className={(JSON.parse(siteConfig('LAYOUT_SIDEBAR_REVERSE')) ? 'flex-row-reverse' : '') + ' w-full mx-auto lg:flex lg:space-x-4 justify-center relative z-10'} >
-                    <div className={`${className || ''} w-full ${fullWidth ? '' : 'max-w-4xl'} h-full overflow-hidden`}>
-
-                        <Transition
-                            show={!onLoading}
-                            appear={true}
-                            enter="transition ease-in-out duration-700 transform order-first"
-                            enterFrom="opacity-0 translate-y-16"
-                            enterTo="opacity-100"
-                            leave="transition ease-in-out duration-300 transform"
-                            leaveFrom="opacity-100 translate-y-0"
-                            leaveTo="opacity-0 -translate-y-16"
-                            unmount={false}
-                        >
-                            {/* 主区上部嵌入 */}
-                            {slotTop}
-
-                            {children}
-                        </Transition>
-                    </div>
-
-                    {/* 右侧栏 */}
-                    <SideRight {...props} />
-                </div>
-            </main>
-
-            {/* 悬浮菜单 */}
-            <RightFloatArea floatSlot={floatSlot} />
-
-            {/* 页脚 */}
-            <Footer title={siteConfig('TITLE') } />
+          {/* 右侧栏 */}
+          <SideRight {...props} />
         </div>
+      </main>
+
+      {/* 悬浮菜单 */}
+      <RightFloatArea floatSlot={floatSlot} />
+
+      {/* 页脚 */}
+      <Footer title={siteConfig('TITLE')} />
+    </div>
   )
 }
 
@@ -122,9 +122,9 @@ const LayoutIndex = (props) => {
  */
 const LayoutPostList = (props) => {
   return <LayoutBase {...props} className='pt-8'>
-        <SlotBar {...props} />
-        {siteConfig('POST_LIST_STYLE') === 'page' ? <BlogPostListPage {...props} /> : <BlogPostListScroll {...props} />}
-    </LayoutBase>
+    <SlotBar {...props} />
+    {siteConfig('POST_LIST_STYLE') === 'page' ? <BlogPostListPage {...props} /> : <BlogPostListScroll {...props} />}
+  </LayoutBase>
 }
 
 /**
@@ -151,11 +151,11 @@ const LayoutSearch = props => {
   })
 
   return (
-        <LayoutBase {...props} currentSearch={currentSearch} className='pt-8'>
-            {!currentSearch
-              ? <SearchNav {...props} />
-              : <div id="posts-wrapper"> {siteConfig('POST_LIST_STYLE') === 'page' ? <BlogPostListPage {...props} /> : <BlogPostListScroll {...props} />}  </div>}
-        </LayoutBase>
+    <LayoutBase {...props} currentSearch={currentSearch} className='pt-8'>
+      {!currentSearch
+        ? <SearchNav {...props} />
+        : <div id="posts-wrapper"> {siteConfig('POST_LIST_STYLE') === 'page' ? <BlogPostListPage {...props} /> : <BlogPostListScroll {...props} />}  </div>}
+    </LayoutBase>
   )
 }
 
@@ -167,18 +167,18 @@ const LayoutSearch = props => {
 const LayoutArchive = (props) => {
   const { archivePosts } = props
   return <LayoutBase {...props} className='pt-8'>
-        <Card className='w-full'>
-            <div className="mb-10 pb-20 bg-white md:p-12 p-3 min-h-full dark:bg-hexo-black-gray">
-                {Object.keys(archivePosts).map(archiveTitle => (
-                    <BlogPostArchive
-                        key={archiveTitle}
-                        posts={archivePosts[archiveTitle]}
-                        archiveTitle={archiveTitle}
-                    />
-                ))}
-            </div>
-        </Card>
-    </LayoutBase>
+    <Card className='w-full'>
+      <div className="mb-10 pb-20 bg-white md:p-12 p-3 min-h-full dark:bg-hexo-black-gray">
+        {Object.keys(archivePosts).map(archiveTitle => (
+          <BlogPostArchive
+            key={archiveTitle}
+            posts={archivePosts[archiveTitle]}
+            archiveTitle={archiveTitle}
+          />
+        ))}
+      </div>
+    </Card>
+  </LayoutBase>
 }
 
 /**
@@ -193,54 +193,54 @@ const LayoutSlug = props => {
   const targetRef = isBrowser ? document.getElementById('article-wrapper') : null
 
   const floatSlot = <>
-        {post?.toc?.length > 1 && <div className="block lg:hidden">
-            <TocDrawerButton
-                onClick={() => {
-                  drawerRight?.current?.handleSwitchVisible()
-                }}
-            />
-        </div>}
-        {/* 跳转评论按钮 */}
-        <JumpToCommentButton />
-    </>
+    {post?.toc?.length > 1 && <div className="block lg:hidden">
+      <TocDrawerButton
+        onClick={() => {
+          drawerRight?.current?.handleSwitchVisible()
+        }}
+      />
+    </div>}
+    {/* 跳转评论按钮 */}
+    <JumpToCommentButton />
+  </>
 
   return (
-        <LayoutBase {...props} headerSlot={<PostHeader {...props} />} showCategory={false} showTag={false} floatSlot={floatSlot} >
-            <div className="w-full lg:hover:shadow lg:border rounded-t-xl lg:rounded-xl lg:px-2 lg:py-4 bg-white dark:bg-hexo-black-gray dark:border-black article">
-                {lock && <ArticleLock validPassword={validPassword} />}
+    <LayoutBase {...props} headerSlot={<PostHeader {...props} />} showCategory={false} showTag={false} floatSlot={floatSlot} >
+      <div className="w-full lg:hover:shadow lg:border rounded-t-xl lg:rounded-xl lg:px-2 lg:py-4 bg-white dark:bg-hexo-black-gray dark:border-black article">
+        {lock && <ArticleLock validPassword={validPassword} />}
 
-                {!lock && <div id="article-wrapper" className="overflow-x-auto flex-grow mx-auto md:w-full md:px-5 ">
+        {!lock && <div id="article-wrapper" className="overflow-x-auto flex-grow mx-auto md:w-full md:px-5 ">
 
-                    <article itemScope itemType="https://schema.org/Movie" className="subpixel-antialiased overflow-y-hidden" >
-                        {/* Notion文章主体 */}
-                        <section className='px-5 justify-center mx-auto max-w-2xl lg:max-w-full'>
-                            {post && <NotionPage post={post} />}
-                        </section>
+          <article itemScope itemType="https://schema.org/Movie" className="subpixel-antialiased overflow-y-hidden" >
+            {/* Notion文章主体 */}
+            <section className='px-5 justify-center mx-auto max-w-2xl lg:max-w-full'>
+              {post && <NotionPage post={post} />}
+            </section>
 
-                        {/* 分享 */}
-                        <ShareBar post={post} />
-                        {post?.type === 'Post' && <>
-                            <ArticleCopyright {...props} />
-                            <ArticleRecommend {...props} />
-                            <ArticleAdjacent {...props} />
-                        </>}
+            {/* 分享 */}
+            <ShareBar post={post} />
+            {post?.type === 'Post' && <>
+              <ArticleCopyright {...props} />
+              <ArticleRecommend {...props} />
+              <ArticleAdjacent {...props} />
+            </>}
 
-                    </article>
+          </article>
 
-                    <div className='pt-4 border-dashed'></div>
+          <div className='pt-4 border-dashed'></div>
 
-                    {/* 评论互动 */}
-                    <div className="duration-200 overflow-x-auto bg-white dark:bg-hexo-black-gray px-3">
-                        <Comment frontMatter={post} />
-                    </div>
-                </div>}
-            </div>
+          {/* 评论互动 */}
+          <div className="duration-200 overflow-x-auto bg-white dark:bg-hexo-black-gray px-3">
+            <Comment frontMatter={post} />
+          </div>
+        </div>}
+      </div>
 
-            <div className='block lg:hidden'>
-                <TocDrawer post={post} cRef={drawerRight} targetRef={targetRef} />
-            </div>
+      <div className='block lg:hidden'>
+        <TocDrawer post={post} cRef={drawerRight} targetRef={targetRef} />
+      </div>
 
-        </LayoutBase>
+    </LayoutBase>
   )
 }
 
@@ -250,34 +250,30 @@ const LayoutSlug = props => {
  * @returns
  */
 const Layout404 = props => {
-  const router = useRouter()
   useEffect(() => {
-    // 延时3秒如果加载失败就返回首页
-    setTimeout(() => {
-      if (isBrowser) {
-        const article = document.getElementById('notion-article')
-        if (!article) {
-          router.push('/').then(() => {
-            // console.log('找不到页面', router.asPath)
-          })
-        }
+    const timeout = setTimeout(() => {
+      const article = document.getElementById('notion-article');
+      if (!article) {
+        window.location.reload();
       }
-    }, 3000)
-  })
+    }, 10000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
-        <LayoutBase {...props}>
-            <div className="text-black w-full h-screen text-center justify-center content-center items-center flex flex-col">
-                <div className="dark:text-gray-200">
-                    <h2 className="inline-block border-r-2 border-gray-600 mr-2 px-3 py-2 align-top">
-                        404
-                    </h2>
-                    {/* 页面未找到 */}
-                    <div className="inline-block text-left h-32 leading-10 items-center">
-                        <h2 className="m-0 p-0">请重新打开所需的文章</h2>
-                    </div>
-                </div>
-            </div>
-        </LayoutBase>
+    <LayoutBase {...props}>
+      <div className="text-black w-full h-screen text-center justify-center content-center items-center flex flex-col">
+        <div className="dark:text-gray-200">
+          <h2 className="inline-block border-r-2 border-gray-600 mr-2 px-3 py-2 align-top">
+            404
+          </h2>
+          <div className="inline-block text-left h-32 leading-10 items-center">
+            <h2 className="m-0 p-0"> 页面不存在 | 文章渲染超时 | 尝试重新打开 </h2>
+          </div>
+        </div>
+      </div>
+    </LayoutBase>
   )
 }
 
@@ -290,24 +286,24 @@ const LayoutCategoryIndex = props => {
   const { categoryOptions } = props
   const { locale } = useGlobal()
   return (
-        <LayoutBase {...props} className='mt-8'>
-            <Card className="w-full min-h-screen">
-                <div className="dark:text-gray-200 mb-5 mx-3">
-                    <i className="mr-4 fas fa-th" />  {locale.COMMON.CATEGORY}:
+    <LayoutBase {...props} className='mt-8'>
+      <Card className="w-full min-h-screen">
+        <div className="dark:text-gray-200 mb-5 mx-3">
+          <i className="mr-4 fas fa-th" />  {locale.COMMON.CATEGORY}:
+        </div>
+        <div id="category-list" className="duration-200 flex flex-wrap mx-8">
+          {categoryOptions?.map(category => {
+            return (
+              <Link key={category.name} href={`/category/${category.name}`} passHref legacyBehavior>
+                <div className={' duration-300 dark:hover:text-white px-5 cursor-pointer py-2 hover:text-indigo-400'}>
+                  <i className="mr-4 fas fa-folder" />  {category.name}({category.count})
                 </div>
-                <div id="category-list" className="duration-200 flex flex-wrap mx-8">
-                    {categoryOptions?.map(category => {
-                      return (
-                            <Link key={category.name} href={`/category/${category.name}`} passHref legacyBehavior>
-                                <div className={' duration-300 dark:hover:text-white px-5 cursor-pointer py-2 hover:text-indigo-400'}>
-                                    <i className="mr-4 fas fa-folder" />  {category.name}({category.count})
-                                </div>
-                            </Link>
-                      )
-                    })}
-                </div>
-            </Card>
-        </LayoutBase>
+              </Link>
+            )
+          })}
+        </div>
+      </Card>
+    </LayoutBase>
   )
 }
 
@@ -320,18 +316,18 @@ const LayoutTagIndex = props => {
   const { tagOptions } = props
   const { locale } = useGlobal()
   return (
-        <LayoutBase {...props} className='mt-8'>
-            <Card className='w-full'>
-                <div className="dark:text-gray-200 mb-5 ml-4">
-                    <i className="mr-4 fas fa-tag" /> {locale.COMMON.TAGS}:
-                </div>
-                <div id="tags-list" className="duration-200 flex flex-wrap ml-8">
-                    {tagOptions.map(tag => <div key={tag.name} className="p-2">
-                        <TagItemMini key={tag.name} tag={tag} />
-                    </div>)}
-                </div>
-            </Card>
-        </LayoutBase>
+    <LayoutBase {...props} className='mt-8'>
+      <Card className='w-full'>
+        <div className="dark:text-gray-200 mb-5 ml-4">
+          <i className="mr-4 fas fa-tag" /> {locale.COMMON.TAGS}:
+        </div>
+        <div id="tags-list" className="duration-200 flex flex-wrap ml-8">
+          {tagOptions.map(tag => <div key={tag.name} className="p-2">
+            <TagItemMini key={tag.name} tag={tag} />
+          </div>)}
+        </div>
+      </Card>
+    </LayoutBase>
   )
 }
 
