579560a1039c6e939262171b4b839edff131810d
Rylan <1217013295@qq.com>
2024-01-25 15:42:06 +0800

    🔧 添加 Steam 链接

--- blog.config.js
+++ blog.config.js
@@ -32,6 +32,7 @@ const BLOG = {
       CONTACT_INSTAGRAM: process.env.NEXT_PUBLIC_CONTACT_INSTAGRAM || '', // 您的instagram地址
       CONTACT_BILIBILI: process.env.NEXT_PUBLIC_CONTACT_BILIBILI || '', // B站主页
       CONTACT_YOUTUBE: process.env.NEXT_PUBLIC_CONTACT_YOUTUBE || '', // Youtube主页
+      CONTACT_STEAM: process.env.NEXT_PUBLIC_CONTACT_STEAM || '', // Steam 主页
     
       NOTION_HOST: process.env.NEXT_PUBLIC_NOTION_HOST || 'https://www.notion.so', // Notion域名，您可以选择用自己的域名进行反向代理，如果不懂得什么是反向代理，请勿修改此项
     
