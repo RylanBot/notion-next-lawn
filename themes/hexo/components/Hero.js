93074747a5a4dc2fe955ca8facef885bc65831f6
Rylan <1217013295@qq.com>
2024-03-24 19:07:59 +0800

    💄 style: 调整 Hexo 首页大图效果

--- themes/hexo/components/Hero.js
+++ themes/hexo/components/Hero.js
@@ -1,4 +1,3 @@
-// import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import Typed from 'typed.js'
 import CONFIG from '../config'
@@ -28,11 +27,12 @@ const Hero = props => {
       changeType(
         new Typed('#typed', {
           strings: GREETING_WORDS,
-          typeSpeed: 200,
-          backSpeed: 100,
-          backDelay: 400,
+          loop: true,
+          typeSpeed: 100,
+          backSpeed: 50,
+          backDelay: 200,
           showCursor: true,
-          smartBackspace: true
+          smartBackspace: true,
         })
       )
     }
@@ -55,17 +55,16 @@ const Hero = props => {
             id="header" style={{ zIndex: 1 }}
             className="w-full h-screen relative bg-black"
         >
-
             <div className="text-white absolute bottom-0 flex flex-col h-full items-center justify-center w-full ">
                 {/* 站点标题 */}
-                <div className='font-black text-4xl md:text-5xl shadow-text'>{siteConfig('TITLE')}</div>
+                <div className='text-center font-black text-7xl lg:text-8xl shadow-text'>{siteConfig('TITLE')}</div>
                 {/* 站点欢迎语 */}
-                <div className='mt-2 h-12 items-center text-center font-medium shadow-text text-lg'>
+                <div className='mt-12 h-12 items-center text-center font-medium shadow-text text-2xl'>
                     <span id='typed' />
                 </div>
 
                 {/* 首页导航大按钮 */}
-                {/* {siteConfig('HEXO_HOME_NAV_BUTTONS', null, CONFIG) && <NavButtonGroup {...props} />} */}
+                {siteConfig('HEXO_HOME_NAV_BUTTONS', null, CONFIG) && <NavButtonGroup {...props} />}
 
                 {/* 滚动按钮 */}
                 <div onClick={scrollToWrapper} className="z-10 cursor-pointer w-full text-center py-4 text-3xl absolute bottom-10 text-white">
@@ -75,8 +74,8 @@ const Hero = props => {
             </div>
 
             <LazyImage id='header-cover' src={siteInfo?.pageCover}
-                className={`header-cover w-full h-screen object-cover object-center ${siteConfig('HEXO_HOME_NAV_BACKGROUND_IMG_FIXED', null, CONFIG) ? 'fixed' : ''}`} />
-
+                className={`header-cover w-full h-screen object-cover object-center ${siteConfig('HEXO_HOME_NAV_BACKGROUND_IMG_FIXED', null, CONFIG) ? 'fixed' : ''}`} 
+            />
         </header>
   )
 }
