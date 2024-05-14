5d41efc546aed6cecbb891657325849a855123bb
RylanBot <rylanbot@foxmail.com>
2024-05-14 15:21:18 +0800

    ✨ feat: 优化移动端图片加载尺寸

--- components/LazyImage.js
+++ components/LazyImage.js
@@ -1,6 +1,7 @@
-// import { siteConfig } from '@/lib/config'
 import Head from 'next/head';
 import { useEffect, useRef, useState } from 'react';
+
+import { siteConfig } from '@/lib/config';
 
 /**
  * 图片懒加载
@@ -20,24 +21,15 @@ export default function LazyImage({
   onLoad,
   style
 }) {
+  const maxWidth = siteConfig('IMAGE_COMPRESS_WIDTH');
+
   const imageRef = useRef(null);
+
   const [imageLoaded, setImageLoaded] = useState(false);
-
-  const generatePlaceholderURL = (text, width, height) => {
-    const minDimension = Math.min(width || 440, height || 320);
-    const characterWidth = 0.5;
-    const totalCharacterWidth = text.length * characterWidth;
-    const fontSize = Math.floor(minDimension / totalCharacterWidth);
-
-    const placeholderText = encodeURIComponent(text);
-    const sizeParam = (width && height) ? `${width}x${height}` : (width || (height ? `${height}x${height}` : '440x320'));
-
-    return `https://fakeimg.pl/${sizeParam}/?text=${placeholderText}&font=lobster&font_size=${fontSize}`;
-  };
+  const [adjustedSrc, setAdjustedSrc] = useState(placeholderSrc || '');
 
   if (!placeholderSrc) {
-    // placeholderSrc = siteConfig('IMG_LAZY_LOAD_PLACEHOLDER');
-    placeholderSrc = generatePlaceholderURL("Rylan's Blog", width, height);
+    placeholderSrc = generatePlaceholder(width, height);
   }
 
   const handleImageLoad = () => {
@@ -48,12 +40,15 @@ export default function LazyImage({
   };
 
   useEffect(() => {
+    const adjustedImageSrc = adjustImgSize(src, maxWidth);
+    setAdjustedSrc(adjustedImageSrc);
+
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
             const lazyImage = entry.target;
-            lazyImage.src = src;
+            lazyImage.src = adjustedImageSrc;
             observer.unobserve(lazyImage);
           }
         });
@@ -72,9 +67,10 @@ export default function LazyImage({
     };
   }, [src]);
 
+  // 动态添加width、height和className属性，仅在它们为有效值时添加
   const imgProps = {
     ref: imageRef,
-    src: imageLoaded ? src : placeholderSrc,
+    src: imageLoaded ? adjustedSrc : placeholderSrc,
     alt: alt,
     onLoad: handleImageLoad,
     className: imageLoaded ? 'transition-opacity' : 'animate-pulse'
@@ -110,9 +106,52 @@ export default function LazyImage({
       <img {...imgProps} />
       {priority && (
         <Head>
-          <link rel='prefetch' as='image' href={src} />
+          <link rel='prefetch' as='image' href={adjustedSrc} />
         </Head>
       )}
     </>
   );
 }
+
+/**
+ * 根据窗口尺寸决定压缩图片宽度
+ * @param {*} src
+ * @param {*} maxWidth
+ * @returns
+ */
+const adjustImgSize = (src, maxWidth) => {
+  if (!src) {
+    return siteConfig('IMG_LAZY_LOAD_PLACEHOLDER');
+  }
+  const screenWidth = window.screen.width;
+
+  // 屏幕尺寸大于默认图片尺寸，没必要再压缩
+  if (screenWidth > maxWidth) {
+    return src;
+  }
+
+  // 匹配 URL 中的 width 参数
+  const widthRegex = /width=\d+/;
+  // 匹配 URL 中的 w 参数
+  const wRegex = /w=\d+/;
+
+  // 替换 width/w 参数
+  return src
+    .replace(widthRegex, `width=${screenWidth}`)
+    .replace(wRegex, `w=${screenWidth}`);
+};
+
+/**
+ * 生成自定义的占位图片
+ */
+const generatePlaceholder = (width, height) => {
+  const text = "Rylan's Blog";
+  const placeholderText = encodeURIComponent(text);
+
+  const minDimension = Math.min(width || 440, height || 320);
+  const characterWidth = 0.5;
+  const totalCharWidth = text.length * characterWidth;
+  const fontSize = Math.floor(minDimension / totalCharWidth);
+  const sizeParam = (width && height) ? `${width}x${height}` : (width || (height ? `${height}x${height}` : '440x320'));
+  return `https://fakeimg.pl/${sizeParam}/?text=${placeholderText}&font=lobster&font_size=${fontSize}`;
+};
