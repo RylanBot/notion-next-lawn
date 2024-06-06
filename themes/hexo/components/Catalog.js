6c3576f8ce0f67ed9f8307c39ddbc7e971bc5df9
RylanBot <rylanbot@foxmail.com>
2024-06-07 02:08:13 +0800

    🐛 fix: PC 端目录过长

--- themes/hexo/components/Catalog.js
+++ themes/hexo/components/Catalog.js
@@ -69,7 +69,7 @@ const Catalog = ({ toc }) => {
       <div className='w-full pb-2'>
         <Progress />
       </div>
-      <div className='overflow-y-auto max-h-36 lg:max-h-full overscroll-none scroll-hidden' ref={tRef}>
+      <div className='overflow-y-auto max-h-36 lg:max-h-96 overscroll-none scroll-hidden' ref={tRef}>
         <nav className='h-full text-black'>
           {toc.map((tocItem) => {
             const id = uuidToId(tocItem.id);
