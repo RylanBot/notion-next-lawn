913ad0e02576e0dcee3bc8563b1f36f35c435848
Rylan <1217013295@qq.com>
2024-06-22 23:12:09 +0800

    🐛 fix: 表格表头移除默认样式失败

--- hooks/useAdjustStyle.js
+++ hooks/useAdjustStyle.js
@@ -53,27 +53,28 @@ const useAdjustStyle = () => {
 
   // 强行移除 react-notion-x 表格表头第一行样式
   useEffect(() => {
+    const targetCss = `${window.location.origin}/_next/static/css/`
+
     for (let i = 0; i < document.styleSheets.length; i++) {
-      const styleSheet = document.styleSheets[i];
-      if (styleSheet.href && styleSheet.href.includes('http')) {
-        continue; // 忽略指定样式表，避免访问报错
-      }
-      try {
-        const rules = styleSheet.cssRules;
-        for (let j = 0; j < rules.length; j++) {
-          const rule = rules[j];
-          if (rule.selectorText === '.notion-simple-table tr:first-child td') {
-            styleSheet.deleteRule(j);
-            break;
+      const styleSheet = document.styleSheets[i]
+      if (styleSheet?.href?.startsWith(targetCss)) {
+        try {
+          const rules = styleSheet.cssRules
+          for (let j = 0; j < rules.length; j++) {
+            const rule = rules[j]
+            if (rule.selectorText === '.notion-simple-table tr:first-child td') {
+              styleSheet.deleteRule(j)
+              break
+            }
           }
+        } catch (error) {
+          console.error('Error accessing stylesheet:', error, styleSheet.href)
         }
-      } catch (error) {
-        console.error('Error accessing stylesheets:', error);
       }
     }
-    return () => {
-    };
-  }, []);
+
+    return () => {}
+  }, [])
 };
 
 export default useAdjustStyle;
