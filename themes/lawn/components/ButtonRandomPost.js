1684d0119833f966e1795ca48fec7c259ca406d0
Rylan <1217013295@qq.com>
2024-07-03 13:43:52 +0800

    🔧 chore: modify the redirection method for random articles

--- themes/lawn/components/ButtonRandomPost.js
+++ themes/lawn/components/ButtonRandomPost.js
@@ -1,26 +1,25 @@
 import { useGlobal } from '@/hooks/useGlobal';
+import { useRouter } from 'next/router';
 
 /**
  * 随机跳转到一个文章
  */
 export default function ButtonRandomPost(props) {
-  const { allNavPages } = props
-  const { locale } = useGlobal()
+  const { allNavPages } = props;
+  const router = useRouter();
+  const { locale } = useGlobal();
 
   function handleWalkAround() {
-    const slugs = allNavPages.map(page => page.slug)
-    const randomIndex = Math.floor(Math.random() * slugs.length)
-    window.location.href = `/${slugs[randomIndex]}`;
+    const slugs = allNavPages.map((page) => page.slug);
+    const randomIndex = Math.floor(Math.random() * slugs.length);
+    // window.location.href = `/${slugs[randomIndex]}`;
+    router.push(`/${slugs[randomIndex]}`);
   }
 
   return (
-    <div className='group right-float-tooltip-parent'
-      onClick={handleWalkAround}
-    >
-      <i className='fa-solid fa-podcast text-xs'></i>
-      <span className='right-float-tooltip -left-4'>
-        {locale.MENU.WALK_AROUND}
-      </span>
+    <div className="group right-float-tooltip-parent" onClick={handleWalkAround}>
+      <i className="fa-solid fa-podcast text-xs"></i>
+      <span className="right-float-tooltip -left-4">{locale.MENU.WALK_AROUND}</span>
     </div>
-  )
+  );
 }
