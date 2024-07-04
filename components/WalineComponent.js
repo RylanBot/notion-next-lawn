4acaf72660215d992a6361934d931b882b14b222
Rylan <1217013295@qq.com>
2024-07-05 00:58:52 +0800

    🔧 chore: update Waline locale

--- components/WalineComponent.js
+++ components/WalineComponent.js
@@ -1,40 +1,41 @@
-import { useRouter } from 'next/router'
-import { createRef, useEffect } from 'react'
+import { useRouter } from 'next/router';
+import { createRef, useEffect } from 'react';
 
-import { init } from '@waline/client'
-import '@waline/client/dist/waline.css'
+import { init } from '@waline/client';
+import '@waline/client/dist/waline.css';
 
-import { siteConfig } from '@/lib/config'
-import { loadLangFromCookies } from '@/lib/lang'
+import { useGlobal } from '@/hooks/useGlobal';
+import { siteConfig } from '@/lib/config';
 
-let waline = null
-let controller = null
+const path = '';
+let waline = null;
 
-const WalineComponent = props => {
-  const containerRef = createRef()
-  const router = useRouter()
+/**
+ * @see https://waline.js.org/guide/get-started.html
+ */
+const WalineComponent = (props) => {
+  const walineRef = createRef();
+  const router = useRouter();
 
-  const updateWaline = url => {
-    if (url !== '' && waline) {
-      waline.update(props)
+  const { locale } = useGlobal();
+  const LANG = siteConfig('LANG');
+
+  const updateWaline = (url) => {
+    if (url !== path && waline) {
+      waline.update(props);
     }
-  }
+  };
 
   useEffect(() => {
-    const initWaline = () => {
-      if (waline) {
-        waline.destroy()
-      }
-
-      controller = new AbortController()
+    if (!waline) {
       waline = init({
         ...props,
-        el: containerRef.current,
+        el: walineRef.current,
         serverURL: siteConfig('COMMENT_WALINE_SERVER_URL'),
-        lang: loadLangFromCookies(),
+        lang: LANG,
         locale: {
           reactionTitle: '',
-          placeholder: ''
+          placeholder: locale.MAILCHIMP.COMMENT_PLACEHOLDER
         },
         reaction: [
           'https://s2.loli.net/2024/04/03/PmDLpdZNf3b1YK2.png', // fish
@@ -52,29 +53,50 @@ const WalineComponent = props => {
         login: 'disable',
         search: false,
         imageUploader: false,
-        requiredMeta: ['nick'],
-        signal: controller.signal // 将信号传递给 Waline
-      })
+        requiredMeta: ['nick']
+      });
     }
 
-    router.events.on('routeChangeStart', () => {
-      if (controller) {
-        controller.abort() // 中止正在进行的请求
-      }
-    })
+    // 跳转评论
+    router.events.on('routeChangeComplete', updateWaline);
+    const anchor = window.location.hash;
+    if (anchor) {
+      // 选择需要观察变动的节点
+      const targetNode = document.getElementsByClassName('wl-cards')[0];
 
-    initWaline()
+      // 当观察到变动时执行的回调函数
+      const mutationCallback = (mutations) => {
+        for (const mutation of mutations) {
+          const type = mutation.type;
+          if (type === 'childList') {
+            const anchorElement = document.getElementById(anchor.substring(1));
+            if (anchorElement && anchorElement.className === 'wl-item') {
+              anchorElement.scrollIntoView({ block: 'end', behavior: 'smooth' });
+              setTimeout(() => {
+                anchorElement.classList.add('animate__animated');
+                anchorElement.classList.add('animate__bounceInRight');
+                observer.disconnect();
+              }, 300);
+            }
+          }
+        }
+      };
+
+      // 观察子节点 变化
+      const observer = new MutationObserver(mutationCallback);
+      observer.observe(targetNode, { childList: true });
+    }
 
     return () => {
       if (waline) {
-        waline.destroy()
-        waline = null
+        waline.destroy();
+        waline = null;
       }
-      router.events.off('routeChangeComplete', updateWaline)
-    }
-  }, [])
+      router.events.off('routeChangeComplete', updateWaline);
+    };
+  }, []);
 
-  return <div ref={containerRef} />
-}
+  return <div ref={walineRef} />;
+};
 
-export default WalineComponent
+export default WalineComponent;
