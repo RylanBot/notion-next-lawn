3f74ae6313f3950a10d1b1ef75035303332f619f
Rylan <1217013295@qq.com>
2024-11-13 14:03:50 +0800

    💄 style: iframe background color

--- hooks/useAdjustStyle.js
+++ hooks/useAdjustStyle.js
@@ -36,29 +36,6 @@ const useAdjustStyle = () => {
     };
   }, []);
 
-  // 自定义 iframe 组件适配黑夜模式
-  useEffect(() => {
-    const color = isDarkMode ? '#000000' : '#f5f5f5';
-    const subdomain = 'notion-widgets';
-
-    const changeWidgetsColor = () => {
-      const iframes = document.querySelectorAll('.notion-asset-wrapper iframe');
-      iframes.forEach((iframe) => {
-        const hostname = new URL(iframe.src).hostname;
-        if (hostname.includes(subdomain)) {
-          iframe.contentWindow.postMessage(color, '*');
-        }
-      });
-    };
-
-    changeWidgetsColor();
-
-    window.addEventListener('load', changeWidgetsColor);
-    return () => {
-      window.removeEventListener('load', changeWidgetsColor);
-    };
-  }, [isDarkMode]);
-
   // 强行移除 react-notion-x 表格表头第一行样式
   useEffect(() => {
     const targetCss = `${window.location.origin}/_next/static/css/`;
