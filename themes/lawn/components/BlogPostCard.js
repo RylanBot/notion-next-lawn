6504afaa4accc5410974e1c1b6dac8c9e5c47d7f
Rylan <1217013295@qq.com>
2024-11-29 19:12:28 +0800

    🔧 chore: refactor className code format

--- themes/lawn/components/BlogPostCard.js
+++ themes/lawn/components/BlogPostCard.js
@@ -19,10 +19,9 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
   return (
     <div
       className={
-        `${
-          siteConfig('LAWN_POST_LIST_COVER_HOVER_ENLARGE', null, CONFIG)
-            ? ' hover:scale-110 transition-all duration-150'
-            : ''
+        `${siteConfig('LAWN_POST_LIST_COVER_HOVER_ENLARGE', null, CONFIG)
+          ? 'hover:scale-110 transition-all duration-150'
+          : ''
         }` || undefined
       }
     >
@@ -34,12 +33,9 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
         data-aos-once="false"
         data-aos-anchor-placement="top-bottom"
         id="blog-post-card"
-        className={`group md:h-56 w-full flex justify-between md:flex-row flex-col-reverse 
-                    overflow-hidden border-2 border-teal-600 dark:border-teal-500 rounded-lg bg-white dark:bg-lawn-black-gray ${
-                      siteConfig('LAWN_POST_LIST_IMG_CROSSOVER', null, CONFIG) && index % 2 === 1
-                        ? 'md:flex-row-reverse'
-                        : ''
-                    }`}
+        className={`group md:h-56 w-full flex justify-between md:flex-row flex-col-reverse overflow-hidden border-2 border-teal-600 dark:border-teal-500 rounded-lg bg-white dark:bg-lawn-black-gray ${siteConfig('LAWN_POST_LIST_IMG_CROSSOVER', null, CONFIG) && index % 2 === 1
+            ? 'md:flex-row-reverse'
+            : ''}`}
       >
         {/* 文字内容 */}
         <BlogPostCardInfo
@@ -52,11 +48,7 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
 
         {/* 图片封面 */}
         {showPageCover && (
-          <div
-            className={`md:w-5/12 overflow-hidden border-dashed border-teal-600 dark:border-teal-500 max-md:border-b-2 ${
-              index % 2 === 0 ? 'lg:border-l-2' : 'lg:border-r-2'
-            }`}
-          >
+          <div className={`md:w-5/12 overflow-hidden border-dashed border-teal-600 dark:border-teal-500 max-md:border-b-2 ${index % 2 === 0 ? 'lg:border-l-2' : 'lg:border-r-2'}`}>
             <Link href={`${siteConfig('SUB_PATH', '')}/${post.slug}`}>
               <LazyImage
                 priority={index === 1}
--- themes/lawn/components/RightFloatArea.js
+++ themes/lawn/components/RightFloatArea.js
@@ -33,12 +33,8 @@ export default function RightFloatArea({ floatSlot }) {
   }, [scrollListener]);
 
   return (
-    <div
-      className={`${showFloatButton ? 'opacity-100' : 'invisible opacity-0'} 
-      duration-300 transition-all bottom-12 right-1 fixed justify-end z-20 rounded-sm
-      text-white bg-teal-500 dark:bg-teal-600 p-2 lg:scale-125 lg:mr-2`}
-    >
-      <div className={'justify-center flex flex-col items-center cursor-pointer space-y-2'}>
+    <div className={`duration-300 transition-all bottom-12 right-1 fixed justify-end z-20 rounded-sm text-white bg-teal-500 dark:bg-teal-600 p-2 lg:scale-125 lg:mr-2 ${showFloatButton ? 'opacity-100' : 'invisible opacity-0'}`}>
+      <div className='justify-center flex flex-col items-center cursor-pointer space-y-2'>
         <FloatDarkModeButton />
         {floatSlot}
         <JumpToTopButton />
