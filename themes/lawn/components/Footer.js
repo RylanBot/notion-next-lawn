738657f43f1bfa693aa4b8d6c32379f74d709e84
Rylan <1217013295@qq.com>
2024-12-29 10:24:27 +0800

    💄 style(notion): routine refinement

--- themes/lawn/components/Footer.js
+++ themes/lawn/components/Footer.js
@@ -3,56 +3,40 @@ import { siteConfig } from '@/lib/config';
 const Footer = () => {
   const since = siteConfig('SINCE');
   const BEI_AN = siteConfig('BEI_AN');
-  // const TITLE = siteConfig('TITLE');
-  // const VERSION = siteConfig('VERSION')
+  const ANALYTICS_BUSUANZI_ENABLE = siteConfig('ANALYTICS_BUSUANZI_ENABLE');
 
   const d = new Date();
   const currentYear = d.getFullYear();
   const copyrightDate = parseInt(since) < currentYear ? since + '-' + currentYear : currentYear;
 
   return (
-    <footer className="relative dark:bg-black flex-shrink-0 bg-lawn-light-gray justify-center text-center m-auto w-full leading-6 text-gray-600 dark:text-gray-100 text-sm p-6">
-      {/* <DarkModeButton/> */}
-      <i className="fas fa-copyright" /> {`${copyrightDate}`}
-      <span>
-        {/* <i className='mx-2 animate-pulse fas fa-heart' /> */}
-        {/* <a
-          href={siteConfig('LINK')}
-          className='underline font-bold  dark:text-gray-300 '
-        >
-          {siteConfig('AUTHOR')}
-        </a> */}
-        <br />
-        {BEI_AN && (
-          <>
-            <i className="fas fa-shield-alt" />
-            <a href="https://beian.miit.gov.cn/" className="px-1">
-              {BEI_AN}
-            </a>
-            <br />
-          </>
-        )}
-        <span className="hidden busuanzi_container_site_pv mx-1">
-          <i className="fas fa-eye" />
-          <span className="px-1 busuanzi_value_site_pv"> </span>
-        </span>
-        <span className="hidden busuanzi_container_site_uv mx-1">
-          <i className="fas fa-users" />
-          <span className="px-1 busuanzi_value_site_uv"> </span>
-        </span>
-        {/* <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}</h1> */}
-        {/* <p className='text-xs pt-2 text-light-500 dark:text-gray-500'>
-          Powered by
-          <a
-            href='https://github.com/tangly1024/NotionNext'
-            className='dark:text-gray-300 px-1'
-          >
-            NotionNext {VERSION}
+    <footer className="z-10 pt-4 pb-2 flex flex-col items-center justify-center w-full text-sm relative dark:bg-black bg-lawn-light-gray text-gray-600 dark:text-gray-100">
+      <div className="mb-2" >
+        <i className="fas fa-copyright" />
+        <span> {`${copyrightDate}`}</span>
+      </div>
+      {BEI_AN && (
+        <div className="mb-2">
+          <i className="fas fa-shield-alt" />
+          <a href="https://beian.miit.gov.cn/" className="px-1 my-2">
+            {BEI_AN}
           </a>
-        </p> */}
-      </span>
-      <br />
-    </footer>
+          <br />
+        </div>
+      )}
+      {ANALYTICS_BUSUANZI_ENABLE && (
+        <div className='mb-2'>
+          <span className="hidden busuanzi_container_site_pv mx-1">
+            <i className="fas fa-eye" />
+            <span className="px-1 busuanzi_value_site_pv"> </span>
+          </span>
+          <span className="hidden busuanzi_container_site_uv mx-1">
+            <i className="fas fa-users" />
+            <span className="px-1 busuanzi_value_site_uv"> </span>
+          </span>
+        </div>
+      )}
+    </footer >
   );
 };
 
