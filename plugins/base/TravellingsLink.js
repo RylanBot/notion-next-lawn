99d77e06b2f10915266c3f783b37b3f1fd4eff3d
Rylan <rylanbot@qq.com>
2025-03-04 18:23:18 +0800

    🔧 chore(TravellingsLink): fix link exposure for proper crawler detection

--- plugins/base/TravellingsLink.js
+++ plugins/base/TravellingsLink.js
@@ -1,7 +1,8 @@
-import useGlobal from '@/hooks/useGlobal';
 import { useEffect, useRef, useState } from 'react';
+import useGlobal from '@/hooks/useGlobal';
 
 const TRAVEL_IMAGE = 'https://www.travellings.cn/assets/logo.gif';
+
 /**
  * 友链接力
  */
@@ -43,43 +44,41 @@ const TravellingsLink = () => {
         <i className="fa-solid fa-train-subway rainbow-text"></i>
       </button>
 
-      {showModal && (
-        <div className="w-full h-screen fixed inset-0 flex items-center justify-center px-4 z-50">
-          <div className="w-full h-screen fixed inset-0 bg-black opacity-75"></div>
-          <div
-            ref={modalRef}
-            className="w-[425px] bg-zinc-100 dark:bg-zinc-900 p-6 rounded border-4 border-teal-500 relative max-sm:mb-10"
+      <div className={`full h-screen fixed inset-0 flex items-center justify-center px-4 z-50 ${showModal ? 'block' : 'hidden'}`}>
+        <div className="w-full h-screen fixed inset-0 bg-black opacity-75"></div>
+        <div
+          ref={modalRef}
+          className="w-[425px] bg-zinc-100 dark:bg-zinc-900 p-6 rounded border-4 border-teal-500 relative max-sm:mb-10"
+        >
+          <button
+            onClick={() => setShowModal(false)}
+            className="absolute top-0 right-2 m-2 text-zinc-800 hover:text-zinc-500 dark:text-zinc-200"
           >
-            <button
-              onClick={() => setShowModal(false)}
-              className="absolute top-0 right-2 m-2 text-zinc-800 hover:text-zinc-500 dark:text-zinc-200"
-            >
-              <i className="fa-solid fa-xmark"></i>
-            </button>
-            <div className="mb-4">
-              <a
-                rel="noreferrer"
-                target="_blank"
-                href="https://www.travellings.cn/"
-                className="text-lg font-bold mb-3 text-teal-600 dark:text-teal-300 hover:text-teal-500 dark:hover:text-teal-200"
-              >
-                <span className="text-base">#</span> {locale.TRVELLINGS.TITLE}
-              </a>
-              <p className="text-sm font-semibold pb-2 border-b-2 border-dashed text-teal-700 dark:text-teal-400 border-teal-700 dark:border-teal-500">
-                {locale.TRVELLINGS.GUIDE}
-              </p>
-            </div>
+            <i className="fa-solid fa-xmark"></i>
+          </button>
+          <div className="mb-4">
             <a
               rel="noreferrer"
               target="_blank"
-              href="https://www.travellings.cn/go.html"
-              className="float-right cursor-pointer"
+              href="https://www.travellings.cn/"
+              className="text-lg font-bold mb-3 text-teal-600 dark:text-teal-300 hover:text-teal-500 dark:hover:text-teal-200"
             >
-              <img className="w-28 h-7" src={TRAVEL_IMAGE} />
+              <span className="text-base">#</span> {locale.TRVELLINGS.TITLE}
             </a>
+            <p className="text-sm font-semibold pb-2 border-b-2 border-dashed text-teal-700 dark:text-teal-400 border-teal-700 dark:border-teal-500">
+              {locale.TRVELLINGS.GUIDE}
+            </p>
           </div>
+          <a
+            rel="noreferrer"
+            target="_blank"
+            href="https://www.travellings.cn/go.html"
+            className="float-right cursor-pointer"
+          >
+            <img className="w-28 h-7" src={TRAVEL_IMAGE} />
+          </a>
         </div>
-      )}
+      </div>
     </>
   );
 };
