fb0b6493a717b20591d442c0c08621faf9ed6204
Rylan <1217013295@qq.com>
2025-07-01 02:32:31 +0800

    🔧 chore(TravellingsLink): improve modal interaction

--- plugins/base/TravellingsLink.js
+++ plugins/base/TravellingsLink.js
@@ -44,24 +44,28 @@ const TravellingsLink = () => {
         <i className="fa-solid fa-train-subway rainbow-text"></i>
       </button>
 
-      <div className={`full h-screen fixed inset-0 flex items-center justify-center px-4 z-50 ${showModal ? 'block' : 'hidden'}`}>
-        <div className="w-full h-screen fixed inset-0 bg-black opacity-75"></div>
+      <div
+        className={`full h-screen fixed inset-0 flex items-center justify-center px-4 z-50 ${
+          showModal ? 'block' : 'hidden'
+        }`}
+      >
+        <div className="w-full h-screen fixed inset-0 bg-black opacity-75" onClick={() => setShowModal(false)}></div>
         <div
           ref={modalRef}
           className="w-[425px] bg-zinc-100 dark:bg-zinc-900 p-6 rounded border-4 border-teal-500 relative max-sm:mb-10"
         >
           <button
-            onClick={() => setShowModal(false)}
             className="absolute top-0 right-2 m-2 text-zinc-800 hover:text-zinc-500 dark:text-zinc-200"
+            onClick={() => setShowModal(false)}
           >
             <i className="fa-solid fa-xmark"></i>
           </button>
           <div className="mb-4">
             <a
+              className="text-lg font-bold mb-3 text-teal-600 dark:text-teal-300 hover:text-teal-500 dark:hover:text-teal-200"
               rel="noreferrer"
               target="_blank"
               href="https://www.travellings.cn/"
-              className="text-lg font-bold mb-3 text-teal-600 dark:text-teal-300 hover:text-teal-500 dark:hover:text-teal-200"
             >
               <span className="text-base">#</span> {locale.TRVELLINGS.TITLE}
             </a>
@@ -70,10 +74,10 @@ const TravellingsLink = () => {
             </p>
           </div>
           <a
+            className="float-right cursor-pointer"
             rel="noreferrer"
             target="_blank"
             href="https://www.travellings.cn/go.html"
-            className="float-right cursor-pointer"
           >
             <img className="w-28 h-7" src={TRAVEL_IMAGE} />
           </a>
