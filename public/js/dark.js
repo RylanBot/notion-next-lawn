f63ddfdd6445f98fd137203c4988537a49febb41
Rylan <rylanbot@qq.com>
2025-12-30 00:09:40 +0800

    🐛 fix: initialize dark mode based on user preference

--- public/js/dark.js
+++ public/js/dark.js
@@ -1,20 +1,34 @@
 (function () {
-    const storageKey = 'darkMode';
-    const classDark = 'dark';
-    const classLight = 'light';
+  const storageKey = 'darkMode';
+  const classDark = 'dark';
+  const classLight = 'light';
 
-    const setClassOnHtml = (darkMode) => {
-        const htmlElement = document.getElementsByTagName('html')[0];
-        htmlElement.classList.remove(darkMode ? classLight : classDark);
-        htmlElement.classList.add(darkMode ? classDark : classLight);
-    };
-
-    const isLocalDark = window.localStorage.getItem(storageKey);
-    if (isLocalDark !== null) {
-        setClassOnHtml(isLocalDark === 'true');
+  // 切换主题
+  const toggleTheme = (isDark) => {
+    const htmlElement = document.documentElement;
+    if (isDark) {
+      htmlElement.classList.remove(classLight);
+      htmlElement.classList.add(classDark);
     } else {
-        const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-        setClassOnHtml(isDarkMode ? classDark : classLight);
-        window.localStorage.setItem(storageKey, isDarkMode ? classDark : classLight);
+      htmlElement.classList.remove(classDark);
+      htmlElement.classList.add(classLight);
     }
+  };
+
+  const preference = localStorage.getItem(storageKey);
+  if (preference !== null) {
+    // 用户偏好
+    toggleTheme(preference === 'true');
+  } else {
+    // 系统当前值
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    toggleTheme(prefersDark);
+  }
+
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+    if (localStorage.getItem(storageKey) === null) {
+      // 如果没有手动调整过，跟随系统变化
+      toggleTheme(e.matches);
+    }
+  });
 })();
